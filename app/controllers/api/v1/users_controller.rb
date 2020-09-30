b28f83eb44e1afa04bc1ac3de46eafa624321927
jonschacter <schacter.jon@gmail.com>
2020-09-29 18:04:01 -0700

    create user and session controllers

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::UsersController < ApplicationController
+    def create
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
