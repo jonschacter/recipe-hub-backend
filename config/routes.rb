136662b0b80fbde67659ae244256c973a6629a68
jonschacter <schacter.jon@gmail.com>
2020-09-29 18:03:40 -0700

    create session routes for user login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
     namespace :api do
         namespace :v1 do
-            resources :users
+            post "/login", to: "sessions#create"
+            delete "/logout", to: "sessions#destroy"
+            get "/current_user", to: "sessions#show"
+            post "/signup", to: "users#create"
         end
     end
 end
