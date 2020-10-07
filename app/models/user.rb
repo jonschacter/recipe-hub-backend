d8ad3d9c980053ea1474125253b5a9d13dd22dc4
github username <schacter.jon@gmail.com>
2020-10-07 16:23:11 -0700

    generate ingredient model and aupdate relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :categories
+    has_many :recipes
+    has_many :ingredients, through: :recipes
+
     has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_231754) do
+ActiveRecord::Schema.define(version: 2020_10_07_232031) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.string "name"
+    t.string "quantity"
+    t.boolean "have", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
