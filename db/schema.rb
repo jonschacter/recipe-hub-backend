a7ab4595ccc31acecb32797891bf22fdf533c9a5
github username <schacter.jon@gmail.com>
2020-10-07 16:18:39 -0700

    generate recipe model and add relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_231532) do
+ActiveRecord::Schema.define(version: 2020_10_07_231754) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "category_id"
+    t.string "name"
+    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
