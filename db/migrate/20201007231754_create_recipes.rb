f305ae6f95d52502ae4b386d86542e325afa9468
github username <schacter.jon@gmail.com>
2020-10-08 14:08:04 -0700

    remove user foreign key

--- db/migrate/20201007231754_create_recipes.rb
+++ db/migrate/20201007231754_create_recipes.rb
@@ -1,8 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
-      t.integer :user_id
-      t.integer :category_id
+      t.belongs_to :category
       t.string :name
       t.integer :rating
 
