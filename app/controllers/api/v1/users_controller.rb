ad52ba3a9d4913632ec1699daf35fe1d79aeb331
github username <schacter.jon@gmail.com>
2020-10-14 15:47:07 -0700

    create users#create logic for user signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::UsersController < ApplicationController
     def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: user, serializer: UserSerializer
+        else
+            render :json => {
+                error: user.errors.full_messages.to_sentence
+            }
+        end
     end
 
     private
