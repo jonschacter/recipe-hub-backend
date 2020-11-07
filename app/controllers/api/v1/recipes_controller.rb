bca57762fc2d38f5fa3af9f69ad7bb64ec0f8fc4
github username <schacter.jon@gmail.com>
2020-11-06 16:27:29 -0800

    group recipe index by categories

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::RecipesController < ApplicationController
     def index
         if logged_in?
-            recipes = current_user.recipes
-            render json: recipes, each_serializer: RecipeSerializer
+            categories = current_user.categories
+            render json: categories, each_serializer: CategorySerializer
         else
             render :json => {
                 error: "You need to log in to view this"
