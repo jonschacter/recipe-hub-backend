5e34ceb00cec07129ef8d5136670999497e0b9e0
github username <schacter.jon@gmail.com>
2020-11-06 17:13:48 -0800

    adjust recipe serializer to include ingredients and category

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,7 @@
 class RecipeSerializer < ActiveModel::Serializer
     attributes :id, :name, :rating
+
+    has_one :category, serializer: CategorySerializer
+
+    has_many :ingredients, serializer: IngredientSerializer
 end
